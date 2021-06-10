28d6f2289d654b14fb3dfbe56ebbbaab397f034b
alicia-granados <aliciagranados35@gmail.com>
2021-06-09 22:27:15 -0500

    docs: :art: Contenedor F - VUE ( Code Documentation)

--- Ordinario/vue/src/views/Library.vue
+++ Ordinario/vue/src/views/Library.vue
@@ -1,6 +1,7 @@
 <template >
     <div class="catalogo  table-responsive">
         <br/>
+        <!-- A table to view books -->
         <table class="table table-bordered table-dark  ">
             <thead>
                 <tr>
@@ -17,8 +18,10 @@
                 </tr>
             </thead>
             <tbody>
+                <!-- Generates a for loop to obtain each variable a book and shows all the books that exist -->
                 <tr :to="{name: 'Book', params: { id: item } }"
                     v-for= "(item,index) of Book" v-bind:key="index" >
+                <!-- Accessing each value in the book through a for loop and assigning it -->
                 <th scope="row">{{index}}</th>
                 <td>{{ item.title }}</td>
                 <td>{{ item.subtitle }}</td>
@@ -29,6 +32,7 @@
                 <td>{{ item.publishedDate }}</td>
                 <td>{{ item.categories }} </td>
                 <td id ="ruta"><br/>
+                    <!-- call the delete function and send the id item of the book -->
                     <button
                         type="button"
                         class="btn btn-danger btn-sm"
@@ -36,27 +40,25 @@
                       Delete
                     </button>
                     <br/><br/>
+                    <!-- Return a view to see a book, and send the id of the book -->
                     <button type="button"
                         class="btn btn-light btn-sm" >
                         <router-link  :to="{name:'bookid', params:{id:item._id.$oid}}">View</router-link> 
                        
                     </button> <br/><br/>
+                    <!-- Return a view to update a book and send the id of the book -->
                     <button  type="button"
                         class="btn btn-light btn-sm " >
-                        <router-link  :to="{name:'bookidup', params:{id:item._id.$oid}}">Update</router-link> 
-                       
-                    </button>
-                    
-                </td>
-            
-                </tr>
-                
+                        <router-link  :to="{name:'bookidup', params:{id:item._id.$oid}}">Update</router-link>   
+                    </button>                 
+                </td>      
+                </tr>        
             </tbody>
         </table>
     </div>
 </template>
 <style>
-
+    /* Add styles to HTML*/
     #ruta a {
     font-weight: small;
     color: #f02a68;
@@ -74,40 +76,43 @@
     },
     data(){
         return {
-            Book:[]
+            Book:[] // Returns a data array for the component instance.
         }
     }, mounted(){
        
+        // Access and manipulate the API using the HTTP method
         fetch(`http://localhost:5001/libros`)
             .then((res) =>  {
                 res.json().then(data => {
-                    data.forEach(dt => this.Book.push(dt));
+                    data.forEach(dt => this.Book.push(dt));  // Returns a promise with the body of the text transformed to JSON and adds a book
                 });
             })
-
-
-            .then((res) => console.log(res))
+            .then((res) => console.log(res)) // Show the promise in the browser console
         .catch(function(e) {
-            console.log(e); 
+            console.log(e);  // In case of an error, show that error in the browser console
         });
     },
      methods:{
+        
+        // Function to DELETE a book
         removeBook(book_id) {
+            // Access and manipulate the API using the HTTP method
             fetch(`http://localhost:5001/libros/${book_id._id.$oid}` , {
             method: 'DELETE',
             })
                 .then(res => {
-                    res.json()
-                    this.Alert();
-                    location.reload()
+                    res.json(); // Returns a promise with the body of the text transformed to JSON
+                    this.Alert(); // Call the function "Alert"
+                    location.reload(); // Reload the page after Updated
                     }) // or res.json() o.text
-                .then(res => console.log(res))
+                .then(res => console.log(res)) // Show the promise in the browser console
             .catch(function(e) {
-                console.log(e); 
+                console.log(e);  // In case of an error, show that error in the browser console
             });
         },
+        
         Alert(){
-            alert('Deleted book')
+            alert('Deleted book'); // Shows an alert after deleted the book
         }    
         
     }
